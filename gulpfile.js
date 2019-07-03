e888a5ad89508fa55c98115df2111348b3967ea7
Diogo Gomes <dgomes@graphnode.com>
2019-07-03 13:29:48 +0100

    Updated cv and improved print version. Updated font awesome.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
+const fs = require('fs');
 const { series, watch, task } = require('gulp');
+const del = require('del');
+const moment = require('moment');
 
-function clean(cb) {
-    // Do nothing.
-    cb && cb();
+async function clean() {
+    const deletedPaths = await del(['index.html']);
+    console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
 }
 
-function build(cb) {
-    const fs        = require('fs');
+async function build() {
+    
     const mustache  = require('mustache');
-    const moment    = require('moment');
 
     let view = {
         firstName: function() { return this.name.split(' ')[0]; },
@@ -24,11 +26,26 @@ function build(cb) {
     let output = mustache.render(template, view);
 
     fs.writeFileSync('index.html', output);
-
-    cb && cb();
 }
 
-function serve(cb) {
+async function buildPdf() {
+    const puppeteer = require('puppeteer');
+
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/', {waitUntil: 'networkidle0'});
+    const pdf = await page.pdf({ 
+        format: 'A4',
+        displayHeaderFooter: false
+    });
+
+    fs.writeFileSync(`download/cv-dgomes-${moment().format('YYYY-MM-DD')}.pdf`, pdf);
+
+    await browser.close();
+    return pdf;
+}
+
+async function serve() {
     const express = require('express');
     const app = express();
     const port = 3000;
@@ -37,7 +54,11 @@ function serve(cb) {
     app.listen(port, () => console.log(`Listening on port ${port}!`));
 
     watch(['./assets/**/*', './data/**/*', './templates/**/*']).on("change", () => build());
+
+    await new Promise(() => {});
 }
 
+exports.clean = clean;
 exports.build = build;
+exports.buildpdf = buildPdf;
 exports.default = series(clean, build, serve);
